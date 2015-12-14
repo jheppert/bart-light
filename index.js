47c70b7464780ffdf2e6696bb2ed476b68887a3e
Jeff <jheppert@gmail.com>
2015-12-14 07:02:55 -0800

    ditched npm bart for parts of npm powl

--- index.js
+++ index.js
@@ -1,31 +1,87 @@
 
 var app = require('express')();
 var http = require('http').Server(app);
-var bart = require('bart').createClient({"inverval":0});
+var parseString = require('xml2js').parseString;
+var request = require('request');
+// var bart = require('bart').createClient({"inverval":0}); // REMOVED FROM BUGGY VERSION
 
 
 function queryBart(req, res){
     console.log("in the function");
 
-    bart.on('mont south', function(data){
-    // console.log(data); //Output raw returned data
-    var trainData = data[0].minutes + "," + data[1].minutes;
-    // var  trainData = [];
-    // for(var i in data) {
-    //   // trainData.push({ "destination": data[i].destination, "minutes": data[i].minutes });
-    //   trainData.push(data[i].minutes);
-    //   // console.log(data[i].destination + " train comes in " + data[i].minutes + " minutes");
-    // }
+    // Begin new test:
+    var timeToStation = 3;
+    var result = {};
+    request('http://api.bart.gov/api/etd.aspx?cmd=etd&orig=deln&' +
+    'key=MW9S-E7SL-26DU-VV8V&dir=s', function(error, response, body) {
 
-    // res.setHeader('Content-Type', 'application/json');
-    res.json(trainData);
-    // server = app.listen(process.env.PORT);
-    // setTimeout(function () {
-    //     server.close();
-    //     // ^^^^^^^^^^^
-    // }, 3000);
+    if (!error && response.statusCode === 200) {
+        // res.json(body);
+      parseString(body, function (error, result) {
+        var departures = [];
+        var response = "";
+        var lines = result.root.station[0].etd;
+        lines.forEach(function(line) {
+          line.estimate.forEach(function(train) {
+            if (train.minutes[0] !== 'Leaving') {
+              departures.push(parseInt(train.minutes[0]));
+            }
+          });
+        });
 
-    });
+        departures.sort(function(a, b) {
+          return a - b;
+        });
+
+        for(var i=0; i<2; i++) {
+            if(departures[i] > 4) {
+                if(i == 1) {
+                    response += ",";
+                }
+                response += departures[i];
+            }
+        }
+
+        res.send(response);
+
+
+
+        
+
+        
+        
+      });
+    } else {
+      console.log(error);
+    }
+});
+
+
+
+
+
+
+
+
+    // bart.on('mont south', function(data){ // REMOVED FROM BUGGY VERSION
+    // // console.log(data); //Output raw returned data
+    // var trainData = data[0].minutes + "," + data[1].minutes; // REMOVED FROM BUGGY VERSION
+    // // var  trainData = [];
+    // // for(var i in data) {
+    // //   // trainData.push({ "destination": data[i].destination, "minutes": data[i].minutes });
+    // //   trainData.push(data[i].minutes);
+    // //   // console.log(data[i].destination + " train comes in " + data[i].minutes + " minutes");
+    // // }
+
+    // // res.setHeader('Content-Type', 'application/json');
+    // res.json(trainData);
+    // // server = app.listen(process.env.PORT);
+    // // setTimeout(function () {
+    // //     server.close();
+    // //     // ^^^^^^^^^^^
+    // // }, 3000);
+
+    // });
 
 }
     
